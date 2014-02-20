60dc7127197e23653d7ea9717ae8d11c876b1ddc
Brian Harrington <brharrington@gmail.com>
2014-02-19 19:15:05 -0800

    add in files missed in previous commit

---
+++ src/main/java/org/rrd4j/graph/AxisImageParameters.java
@@ -0,0 +1,25 @@
+package org.rrd4j.graph;
+
+class AxisImageParameters {
+    //long start, end;
+    boolean logarithmic;
+    double yminval;
+    double ymaxval;
+    int unitsexponent;
+    double base;
+    double magfact;
+    char symbol;
+    double ygridstep;
+    int ylabfact;
+    double decimals;
+    int quadrant;
+    double scaledstep;
+   // int xsize;
+    //int ysize;
+    int axisxorigin;
+    int xoriginVerticalLabel;
+    //int yorigin;
+    //int unitslength;
+    //int xgif, ygif;
+    //String unit;
+}
